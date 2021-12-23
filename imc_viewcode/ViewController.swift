190566a1ca4a87b6096a152546b2526f997fffde
Daniel Araujo <danielaraujos@live.com>
2021-12-23 15:14:12 -0300

    Initial Commit

---
+++ imc_viewcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imc_viewcode
+//
+//  Created by Daniel Araujo on 23/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
