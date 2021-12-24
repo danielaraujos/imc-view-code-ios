df4e45926bb6146ddb70502b96f32fc1da36553f
Daniel Araujo <danielaraujos@live.com>
2021-12-24 11:16:14 -0300

    Change name class ViewController for FirstScreenController

--- imc_viewcode/AppDelegate.swift
+++ imc_viewcode/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
-        let controller = ViewController()
+        let controller = FirstScreenController()
         
         window.rootViewController = controller
         self.window = window
